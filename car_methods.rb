72429c2027b4fa87648faa8287f35a4c1c2a94d5
steph <stephaniesanquinzi@gmail.com>
2016-09-29 13:45:58 +0300

    methods

---
+++ car_methods.rb
@@ -0,0 +1,15 @@
+def accelerate
+ puts "stepping on the gas"
+ puts "speeding up"
+end
+def sound_horn
+ puts "pressing the horn button"
+ puts "Beep beep"
+end
+def use_headlights(brightness)
+ puts "Turning on #{brightness} head;lights"
+ puts "watch out for deer!"
+end
+sound_horn
+accelerate
+use_headlights("high-beam")
